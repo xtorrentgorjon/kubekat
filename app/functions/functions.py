0ee7daa65e27efb6ef1683b5fd3b956e429fdbf1
Xavier Torrent <sendotux@gmail.com>
2019-01-24 23:47:03 +0100

    App now correctly displays deployments in the table instead of dummy data.

--- app/functions/functions.py
+++ app/functions/functions.py
@@ -3,27 +3,33 @@ from kubernetes import client, config, watch
 class kubernetes_query():
     def __init__(self):
         self.__config = config.load_incluster_config()
-    def query(self):
+    def query(self, namespace):
         api = client.AppsV1beta1Api()
-        pod_list = api.list_namespaced_deployment("default")
-        return pod_list
+        deployment_list = api.list_namespaced_deployment(namespace)
+        return deployment_list
 
 class label_checker():
-    def __init__(self, namespaces):
+    def __init__(self, namespaces, app):
         self.__resources_with_label = []
         self.__resources_without_label = []
         self.__namespaces = namespaces
+        self.app = app
 
     def check_namespace(self, namespace):
-        output = [\
-        [namespace, "app1"],\
-        [namespace, "app2"],\
-        [namespace, "app3"]]
+        kq = kubernetes_query()
+        output = kq.query(namespace)
 
-        self.__resources_without_label.append(output)
+        for deployment in output.items:
+            out_deployment = [str(deployment.metadata.namespace),str(deployment.metadata.name)]
+            self.__resources_without_label.append(out_deployment)
+        #out_deployment = [output["items"][0]["metadata"]["namespace"], output["items"][0]["metadata"]["name"]]
+        #out_deployment = [str(output.items[0].metadata.namespace), "NAME"]
+        #self.app.logger.info('%s', out_deployment[0])
+        #self.__resources_without_label.append(out_deployment)
 
     def check_all_namespaces(self):
         for namespace in self.__namespaces:
+            self.app.logger.info('CHECKING NAMESPACE %s', namespace)
             self.check_namespace(namespace)
 
         return self.__resources_without_label
