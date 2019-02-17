edeeb3d9fe2a93a8ef2560cce836924c1e9fbe60
Xavier Torrent <sendotux@gmail.com>
2019-02-17 18:39:57 +0100

    Code improvements 2).

--- app/functions/functions.py
+++ app/functions/functions.py
@@ -46,32 +46,30 @@ class label_checker():
 
     def filter_resource_by_label(self, exist_filter):
         self.__app.logger.debug('Current filter %s', exist_filter)
-        if exist_filter == ['']:
+        if exist_filter == ['']: # If there is no filter, accept all.
             for resource in self.__resource_list:
                 self.__resource_correct_labels.append(resource)
         else:
             for resource in self.__resource_list:
-                self.__app.logger.debug('Checking resource %s', resource)
                 filtered = False
-                for filter in exist_filter:
-                    if ":" in filter:
-                        filter = filter.split(":")
-                        filter_label_key = filter[0].strip()
-                        filter_label_value = filter[1].strip()
-                        if resource["metadata"].labels == None:
-                            filtered = True
-                        else:
+                if resource["metadata"].labels == None: # If resource has no labels, automatically classify it as invalid.
+                    filtered = True
+                else:
+                    self.__app.logger.debug('Checking resource %s', resource)
+                    for filter in exist_filter:
+                        if ":" in filter: # If : exists, must check full key:value pair.
+                            filter = filter.split(":")
+                            filter_label_key = filter[0].strip()
+                            filter_label_value = filter[1].strip()
                             if filter_label_key not in resource["metadata"].labels:
                                 filtered = True
                             elif str(filter_label_value) == str(resource["metadata"].labels[filter_label_key]):
                                 filtered = False
                             else:
                                 filtered = True
-                    else:
-                        if resource["metadata"].labels == None:
-                            filtered = True
-                        elif filter not in resource["metadata"].labels:
-                            filtered = True
+                        else: # Check only that label exists.
+                            if filter not in resource["metadata"].labels:
+                                filtered = True
                 if filtered:
                     self.__resource_incorrect_labels.append(resource)
                 else:
@@ -79,17 +77,10 @@ class label_checker():
 
 
     def check_namespace(self, namespace):
-        output_deployments = self.__kubernetes_query.list_deployments_in_namespace(namespace)
-        output_statefulsets = self.__kubernetes_query.list_statefulsets_in_namespace(namespace)
-
-        output = []
-
-        for deployment in output_deployments:
-            output.append({"metadata":deployment, "type": "deployment"})
-
-        for statefulset in output_statefulsets:
-            output.append({"metadata":statefulset, "type": "statefulset"})
-
+        output = [{"metadata": deployment, "type": "deployment"}
+            for deployment in self.__kubernetes_query.list_deployments_in_namespace(namespace)]
+        output += [{"metadata": statefulset, "type": "statefulset"}
+            for statefulset in self.__kubernetes_query.list_statefulsets_in_namespace(namespace)]
         return output
 
 
@@ -98,7 +89,6 @@ class label_checker():
 
         for namespace in output:
             self.__app.logger.debug('Checking namespace %s', namespace)
-            resources_in_namespace = self.check_namespace(namespace)
-            self.__resource_list += resources_in_namespace
+            self.__resource_list += self.check_namespace(namespace)
 
         return self.__resource_list
