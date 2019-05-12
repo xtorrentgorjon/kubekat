669126d3603e8383a4e556341e32abad8ead2381
Xavier Torrent <sendotux@gmail.com>
2019-05-12 21:54:42 +0200

    Changed import.

--- library/kubekat_ui/app/test_app.py
+++ library/kubekat_ui/app/test_app.py
@@ -1,11 +1,11 @@
 import unittest
-import app
+from app import string_to_list
 
 class TestApp(unittest.TestCase):
     def test_unittest_string_to_list(self):
         input_string = "sla, teststr :  value1, maintainer:value2   ,, team     :SRE"
         output_compare = ["sla", "teststr:value1", "maintainer:value2", "team:SRE"]
-        output_list = app.string_to_list(input_string)
+        output_list = string_to_list(input_string)
         self.assertEqual(output_list, output_compare)
 
 if __name__ == '__main__':
